2ce3768b1d83a9bbd3d7ae1e22a2cbf4f74fe345
Aidan Feldman <aidan.feldman@gmail.com>
2011-05-13 10:38:04 -0400

    use transparent virtual pixels

--- mustachio.js
+++ mustachio.js
@@ -79,7 +79,7 @@ exports.processSrc = function(src, res){
 
 exports.mustachify = function(filename, photoData, res){
   var i;
-  var convertArgs = [filename]
+  var convertArgs = [filename, '-virtual-pixel', 'Transparent']
   photoData.tags.forEach(function(face){
     // perform affine transform, such that the top-center
     // of the mustache is mapped to the nose, and the bottom-center
