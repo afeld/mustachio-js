678e6398c1eb65c05cec45a80f5411f3bccaa860
Aidan Feldman <aidan.feldman@gmail.com>
2011-05-13 23:07:07 -0400

    put -matte param within layer

--- mustachio.js
+++ mustachio.js
@@ -79,7 +79,7 @@ exports.processSrc = function(src, res){
 
 exports.mustachify = function(filename, photoData, res){
   var i;
-  var convertArgs = [filename,, '-matte', '-virtual-pixel', 'Transparent']
+  var convertArgs = [filename, '-virtual-pixel', 'Transparent']
   photoData.tags.forEach(function(face){
     // perform affine transform, such that the top-center
     // of the mustache is mapped to the nose, and the bottom-center
@@ -95,7 +95,7 @@ exports.mustachify = function(filename, photoData, res){
     var affineParamsStr = affineParams.map( function(subAry){ return subAry.join(','); } ).join(' ');
     
     convertArgs = convertArgs.concat(
-      ['(', MUSTACHE.filename, '+distort', 'Affine', affineParamsStr, ')']
+      ['(', MUSTACHE.filename, '-matte', '+distort', 'Affine', affineParamsStr, ')']
     );
   });
   convertArgs = convertArgs.concat(['-flatten', '-']);
